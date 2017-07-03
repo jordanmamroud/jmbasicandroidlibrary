1037fdef012465dd842bb940799ba699155eb53b
jordan mamroud <jordanmamroud@gmail.com>
2017-07-03 10:10:10 -0400

    initial commit

---
+++ app/src/main/java/com/example/jordan/basicslibrary/Test.java
@@ -0,0 +1,11 @@
+package com.example.jordan.basicslibrary;
+
+/**
+ * Created by Jordan on 7/3/2017.
+ */
+
+public class Test {
+    public void tert(){
+        System.out.println("tert");
+    }
+}
